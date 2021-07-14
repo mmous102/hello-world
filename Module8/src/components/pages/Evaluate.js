0a3b5d0b91c0d285be2ee065ac8bc345d35b64da
Mary Mousa <mmous102@uottawa.ca>
2021-07-14 16:43:48 -0400

    changes

---
+++ Module8/src/components/pages/Evaluate.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Evaluate() {
+  return <h1 className='Evaluate'></h1>;
+}
---
+++ Module8/src/components/pages/Register.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../App.css';
+
+import { Link } from 'react-router-dom';
+
+export default  function Register() {
+return <h1 className='Register'></h1>;
+}
