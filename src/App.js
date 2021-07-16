62094b1676566867a68926a3588605929494ca4e
maryankhalil <maryanabdelnour24@gmail.com>
2021-07-16 19:19:10 -0400

    Revert "register"
    This reverts commit 1bb6a734a057ab54c85f1600751c05b3e3b9cb8d.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Evaluate from './components/pages/Evaluate';
 import Activities from './components/pages/Activities';
-import signup from './components/pages/register';
+import signup from './components/pages/sign-up';
 
 import SurveyComponent from './components/pages/Evaluate' 
-import register from './components/pages/register';
 
 function App() {
   return (
@@ -19,7 +18,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/Evaluate' component={Evaluate} />
           <Route path='/Activities' component={Activities} />
-          <Route path='/register' component={register} />
+          <Route path='/sign-up' component={signup} />
         </Switch>
       </Router>
     </>
